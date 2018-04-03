b192fb7b56cd858365f6f7fb2aecb9652608b2a2
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-04-03 14:05:37 -0700

    testing again

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 require('dotenv').config(); 
+const env = process.env.NODE_ENV || 'development'; 
 
 module.exports = {
-    DATABASE_URL: process.env.DATABASE_URL, 
+    DATABASE_URL: 'postgres://coxaffrf:7Dli2nfv3MbIojjet7iXTuzq30RxpyUf@stampy.db.elephantsql.com:5432/coxaffrf', 
     PORT: process.env.PORT || 8080, 
     SEQUELIZE_OPTIONS: {logging: env === 'test' ? false : console.log}, 
     JWT_SECRET: process.env.JWT_SECRET, 
